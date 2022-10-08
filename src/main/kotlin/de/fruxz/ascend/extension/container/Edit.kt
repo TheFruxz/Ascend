883995d23a40796ba387a70d08c89b6b08f76e0f
TheFruxz <cedricspitzer@outlook.de>
2022-10-08 11:16:29 +0200

    fix: uninteded overload

--- src/main/kotlin/de/fruxz/ascend/extension/container/Edit.kt
+++ src/main/kotlin/de/fruxz/ascend/extension/container/Edit.kt
@@ -10,24 +10,6 @@ package de.fruxz.ascend.extension.container
 fun <I, O> Map<I, O>.edited(builder: MutableMap<I, O>.() -> Unit) = toMutableMap().apply(builder).toMap()
 
 /**
- * This function takes the input [List], converts it into a [MutableList] using
- * the [toMutableList] function, applies the [builder] function to it and returns
- * the result as a [List].
- * @author Fruxz
- * @since 1.0
- */
-fun <O> List<O>.edited(builder: MutableList<O>.() -> Unit) = toMutableList().apply(builder).toList()
-
-/**
- * This function takes the input [Set], converts it into a [MutableSet] using
- * the [toMutableSet] function, applies the [builder] function to it and returns
- * the result as a [Set].
- * @author Fruxz
- * @since 1.0
- */
-fun <O> Set<O>.edited(builder: MutableSet<O>.() -> Unit) = toMutableSet().apply(builder).toSet()
-
-/**
  * This function takes the input [Iterable], converts it into a [MutableList] using
  * the [toMutableList] function, applies the [builder] function to it and returns
  * the result as a [List].
