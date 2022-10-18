101988f03380c9a6ca4a7797e56838ea475e03fb
TheFruxz <cedricspitzer@outlook.de>
2022-10-18 18:05:24 +0200

    feature: add copyTo & downloadTo functions

---
+++ src/main/kotlin/de/fruxz/ascend/extension/network/URL.kt
@@ -0,0 +1,33 @@
+package de.fruxz.ascend.extension.network
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.URL
+import java.nio.file.CopyOption
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+
+/**
+ * This function downloads the content of the given [URL] to the given [destination] path.
+ * @param destination The destination path to download the content to.
+ * @param options The [CopyOption]s to use when copying the content.
+ * @return The number of bytes that have been copied.
+ * @author Fruxz
+ * @since 1.0
+ */
+suspend inline fun URL.copyTo(destination: Path, vararg options: CopyOption) =
+	withContext(Dispatchers.IO) {
+		Files.copy(openStream(), destination, *options)
+	}
+
+/**
+ * This function downloads the content of the given [URL] to the given [destination] path.
+ * If the file already exists, it will be replaced.
+ * @param destination The destination path to download the content to.
+ * @return The number of bytes that have been copied.
+ * @author Fruxz
+ * @since 1.0
+ */
+suspend inline infix fun URL.downloadTo(destination: Path) =
+	copyTo(destination, StandardCopyOption.REPLACE_EXISTING)
