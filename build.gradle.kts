d30f36e850e4db085b1570c369244dd41bd25b25
TheFruxz <cedricspitzer@outlook.de>
2022-11-28 21:26:41 +0100

    chore: remove factory logger (slf4j)

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.slf4j:slf4j-api:2.0.5")
 
     implementation("org.jetbrains.exposed:exposed-core:0.41.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
--- src/main/kotlin/de/fruxz/ascend/extension/logging/Logger.kt
+++ src/main/kotlin/de/fruxz/ascend/extension/logging/Logger.kt
@@ -1,6 +1,5 @@
 package de.fruxz.ascend.extension.logging
 
-import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 import java.util.logging.Logger as JavaUtilLogger
 
@@ -37,26 +36,6 @@ fun getLogger(kclass: KClass<*>) =
 	getLogger(kclass.simpleName ?: "generic")
 
 /**
- * Creates a logger from the [LoggerFactory] using the given name.
- * @param name the name of the logger
- * @return the logger
- * @see LoggerFactory.getLogger
- * @author Fruxz
- * @since 1.0
- */
-fun getFactoryLogger(name: String) = LoggerFactory.getLogger(name)!!
-
-/**
- * Creates a logger from the [LoggerFactory] using the given class.
- * @param kclass the class
- * @return the logger
- * @see LoggerFactory.getLogger
- * @author Fruxz
- * @since 1.0
- */
-fun getFactoryLogger(kclass: KClass<*>) = LoggerFactory.getLogger(kclass.java)!!
-
-/**
  * Creates a logger from the [JavaUtilLogger] using the class of the given object.
  * @return the logger
  * @see getLogger
@@ -64,12 +43,3 @@ fun getFactoryLogger(kclass: KClass<*>) = LoggerFactory.getLogger(kclass.java)!!
  * @since 1.0
  */
 fun <T : Any> T.getItsLogger() = getLogger(this::class)
-
-/**
- * Creates a logger from the [LoggerFactory] using the class of the given object.
- * @return the logger
- * @see getFactoryLogger
- * @author Fruxz
- * @since 1.0
- */
-fun <T : Any> T.getItsFactoryLogger() = getFactoryLogger(this::class)
