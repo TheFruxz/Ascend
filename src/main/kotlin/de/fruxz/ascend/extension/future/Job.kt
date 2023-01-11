e7ca1545f70e22e78ffd6477df97b5d4713abe56
TheFruxz <cedricspitzer@outlook.de>
2023-01-11 10:00:11 +0100

    feature: add mapOnCompletion function for Deferred

--- src/main/kotlin/de/fruxz/ascend/extension/future/Job.kt
+++ src/main/kotlin/de/fruxz/ascend/extension/future/Job.kt
@@ -1,8 +1,32 @@
 package de.fruxz.ascend.extension.future
 
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 
 /**
  * This function is a wrapper for [Job.join]!
  */
 suspend fun Job.await() = this.join()
+
+/**
+ * This function allows to edit the result of [this] deferred on completion,
+ * via the [process] function. Then a new [CompletableDeferred] object will
+ * be created, to return the new result.
+ * This helps to easily edit/map results, without having to directly [Deferred.await]
+ * on them.
+ * @param process is the process, to transform/map the result, like [Deferred.invokeOnCompletion], it is executed synchronously.
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <I, O> Deferred<I>.mapOnCompletion(process: (I) -> O) = CompletableDeferred<O>().also {
+
+	@OptIn(ExperimentalCoroutinesApi::class)
+	invokeOnCompletion { exception ->
+		when (exception) {
+			null -> it.complete(process(this.getCompleted()))
+			else -> it.completeExceptionally(exception)
+		}
+	}
+}
