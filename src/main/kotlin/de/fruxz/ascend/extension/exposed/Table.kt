d0237f1adfa68c4a228834639919d7c75af62053
TheFruxz <cedricspitzer@outlook.de>
2023-01-26 02:23:44 +0100

    feature: add improved table management functions

--- src/main/kotlin/de/fruxz/ascend/extension/exposed/Table.kt
+++ src/main/kotlin/de/fruxz/ascend/extension/exposed/Table.kt
@@ -8,4 +8,23 @@ import org.jetbrains.exposed.sql.Table
  * @author Fruxz
  * @since 1.0
  */
+@Deprecated("A better and more reliable way is possible!", ReplaceWith("initialize()"))
 fun Table.createTable() = SchemaUtils.create(this)
+
+/**
+ * This function creates or updates [this] table in the database.
+ * This utilizes the [SchemaUtils.createMissingTablesAndColumns] function.
+ * @author Fruxz
+ * @since 1.0
+ */
+fun Table.initialize(withLogs: Boolean = true) =
+	SchemaUtils.createMissingTablesAndColumns(this, withLogs = withLogs)
+
+/**
+ * This function creates or updates the [tables] in the database.
+ * This utilizes the [SchemaUtils.createMissingTablesAndColumns] function.
+ * @author Fruxz
+ * @since 1.0
+ */
+fun initializeTables(vararg tables: Table, withLogs: Boolean = true) =
+	SchemaUtils.createMissingTablesAndColumns(*tables, withLogs = withLogs)
