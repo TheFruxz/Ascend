62f3bebf9c94c706ab2f3e24929a355e9adf4219
TheFruxz <cedricspitzer@outlook.de>
2023-01-29 17:12:27 +0100

    fix: remove replaced extension function
    This feature now exists directly inside Kotlin, so they is no need for this anymore

--- src/main/kotlin/de/fruxz/ascend/extension/container/Checks.kt
+++ src/main/kotlin/de/fruxz/ascend/extension/container/Checks.kt
@@ -2,14 +2,6 @@ package de.fruxz.ascend.extension.container
 
 /**
  * This function returns, if at least one element matches the [string] with [ignoreCase] in mind.
- * This utilizes the [String.equals] function in the [Iterable.any] process.
- * @author Fruxz
- * @since 1.0
- */
-fun Iterable<String>.contains(string: String, ignoreCase: Boolean) = any { it.equals(string, ignoreCase) }
-
-/**
- * This function returns, if at least one element matches the [string] with [ignoreCase] in mind.
  * This utilizes the [String.equals] function in the [Array.any] process.
  * @author Fruxz
  * @since 1.0
