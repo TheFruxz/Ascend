95a38c7b4dcdbca240b9ce354c89318b77c68f6e
TheFruxz <cedricspitzer@outlook.de>
2023-02-06 23:35:36 +0100

    chore: switch color short-functions to awt colors

--- src/main/kotlin/de/fruxz/ascend/extension/data/Color.kt
+++ src/main/kotlin/de/fruxz/ascend/extension/data/Color.kt
@@ -1,16 +1,11 @@
 package de.fruxz.ascend.extension.data
 
-import de.fruxz.ascend.tool.color.Color
 import java.awt.Color as AwtColor
 
-fun rgb(rgb: Int) = Color.of(rgb)
+fun rgb(rgb: Int) = AwtColor(rgb)
 
-fun rgb(red: Int, green: Int, blue: Int) = Color.of(red, green, blue)
+fun rgb(red: Int, green: Int, blue: Int) = AwtColor(red, green, blue)
 
-fun rgbAwt(red: Int, green: Int, blue: Int) = AwtColor(red, green, blue)
+fun rgb(red: Int, green: Int, blue: Int, alpha: Int) = AwtColor(red, green, blue, alpha)
 
-fun rgbAwt(rgb: Int) = AwtColor(rgb)
-
-fun hsb(h: Float, s: Float, b: Float) = Color.of(hsbAwt(h, s, b))
-
-fun hsbAwt(h: Float, s: Float, b: Float): AwtColor = AwtColor.getHSBColor(h, s, b)
+fun hsb(h: Float, s: Float, b: Float) = AwtColor(h, s, b)
