87aa76e88b2da23edd975f79a6689c6810a7f5cc
TheFruxz <cedricspitzer@outlook.de>
2023-02-19 20:25:19 +0100

    chore: deprecate own Base64 util class

--- src/main/kotlin/de/fruxz/ascend/tool/base64/Base64.kt
+++ src/main/kotlin/de/fruxz/ascend/tool/base64/Base64.kt
@@ -1,26 +1,38 @@
 package de.fruxz.ascend.tool.base64
 
-import java.util.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.io.encoding.Base64 as KotlinBase64
 
 /**
  * This object provides methods to encode and decode strings to and from Base64.
  * @author Fruxz
  * @since 1.0
  */
+@Suppress("UnnecessaryOptInAnnotation") // This is necessary for the opt-in
+@OptIn(ExperimentalEncodingApi::class)
+@Deprecated("This API is replaced with Kotlin's Base64 API", ReplaceWith("Base64", "kotlin.io.encoding.Base64"))
 object Base64 {
 
-	/**
-	 * From String to Base64
-	 * @author Fruxz
-	 * @since 1.0
-	 */
-	fun encodeToString(input: String): String = Base64.getEncoder().encodeToString(input.encodeToByteArray())
+    /**
+     * From String to Base64
+     * @author Fruxz
+     * @since 1.0
+     */
+    @Deprecated(
+        message = "Use Kotlin Library instead",
+        replaceWith = ReplaceWith("Base64.encode(input.encodeToByteArray())", "kotlin.io.encoding.Base64"),
+    )
+    fun encodeToString(input: String): String = KotlinBase64.encode(input.encodeToByteArray())
 
-	/**
-	 * From Base64 to String
-	 * @author Fruxz
-	 * @since 1.0
-	 */
-	fun decodeToString(input: String): String = Base64.getDecoder().decode(input.encodeToByteArray()).decodeToString()
+    /**
+     * From Base64 to String
+     * @author Fruxz
+     * @since 1.0
+     */
+    @Deprecated(
+        message = "Use Kotlin Library instead",
+        replaceWith = ReplaceWith("Base64.decode(input.encodeToByteArray()).decodeToString()", "kotlin.io.encoding.Base64"),
+    )
+    fun decodeToString(input: String): String = KotlinBase64.decode(input.encodeToByteArray()).decodeToString()
 
 }
