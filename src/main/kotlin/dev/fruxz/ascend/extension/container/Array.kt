759599075b4a6e458b44b2afa2429f0f63b05e6a
TheFruxz <cedricspitzer@outlook.de>
2023-04-10 12:26:30 +0200

    chore: properly import LanguageFeature in Array.kt

--- src/main/kotlin/dev/fruxz/ascend/extension/container/Array.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/container/Array.kt
@@ -2,26 +2,26 @@ package dev.fruxz.ascend.extension.container
 
 import dev.fruxz.ascend.annotation.LanguageFeature
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Int.Companion.get(vararg int: Int) = arrayOf(*int.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Long.Companion.get(vararg long: Long) = arrayOf(*long.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Float.Companion.get(vararg float: Float) = arrayOf(*float.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Double.Companion.get(vararg double: Double) = arrayOf(*double.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Boolean.Companion.get(vararg boolean: Boolean) = arrayOf(*boolean.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Char.Companion.get(vararg char: Char) = arrayOf(*char.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Byte.Companion.get(vararg byte: Byte) = arrayOf(*byte.toTypedArray())
 
-@dev.fruxz.ascend.annotation.LanguageFeature
+@LanguageFeature
 operator fun Short.Companion.get(vararg short: Short) = arrayOf(*short.toTypedArray())
