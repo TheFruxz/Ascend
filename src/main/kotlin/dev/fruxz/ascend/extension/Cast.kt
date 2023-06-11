0132be91727da1871cf6c3450c6a2e9be4e6e803
Fruxz <cedricspitzer@outlook.de>
2023-06-11 22:35:39 +0200

    chore: remove unnecessary Nothing.forceCast functions

--- src/main/kotlin/dev/fruxz/ascend/extension/Cast.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/Cast.kt
@@ -39,46 +39,6 @@ fun <O> Any?.forceNullableCast() = this as O?
 fun <O> Any?.forceNullableCastOrNull() = tryOrNull { this as O? }
 
 /**
- * Cast the given object to the given type and not highlight risky casts in the IDE.
- * @param O the type of the object to cast
- * @return the cast object
- * @author Fruxz
- * @since 1.0
- */
-fun <O> Nothing?.forceCast() = this as O
-
-/**
- * Cast the given object to the given type or null if fails and not highlight risky casts in the IDE.
- * @param O the type of the object to cast
- * @return the cast object
- * @author Fruxz
- * @since 1.0
- */
-fun <O> Nothing?.forceCastOrNull() = tryOrNull { this as O }
-
-/**
- * Cast the given object to the given type and not highlight risky casts in the IDE.
- * @param O the type of the object to cast
- * @return the cast object
- * @author Fruxz
- * @since 1.0
- */
-fun <O> Nothing?.forceNullableCast() = this as O?
-
-/**
- * Cast the given object to the given type or null if fails and not highlight risky casts in the IDE.
- * @param O the type of the object to cast
- * @return the cast object
- * @author Fruxz
- * @since 1.0
- */
-fun <O> Nothing?.forceNullableCastOrNull() = try {
-	this
-} catch (e: ClassCastException) {
-	null
-}
-
-/**
  * Throws away the object by returning [Unit]
  * @return [Unit]
  * @author Fruxz
