60da2802c397df62e06d1dad2fd924dbeba61648
Fruxz <cedricspitzer@outlook.de>
2023-07-05 23:37:30 +0200

    Update URL function to handle deprecation
    Modified the 'url' function in URL.kt to handle the deprecation of 'URL(...)'. Now 'uri' function converts the given string to a 'URI' object before converting it to a 'URL' object. This change was made to align with updates in Java 20 where the 'URL(...)' constructor has been deprecated.

--- src/main/kotlin/dev/fruxz/ascend/extension/data/URL.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/data/URL.kt
@@ -2,8 +2,25 @@ package dev.fruxz.ascend.extension.data
 
 import org.intellij.lang.annotations.Language
 import java.net.URI
+import java.net.URISyntaxException
 import java.net.URL
 
-fun url(@Language("url") url: String) = URL(url)
+/**
+ * Converts a given URL string to a `URL` object.
+ *
+ * @param url The string representing the URL.
+ * @return A `URL` object representing the specified URL string.
+ * @throws URISyntaxException If the URL syntax is invalid.
+ */
+@Throws(URISyntaxException::class)
+fun url(@Language("url") url: String) = uri(url).toURL() // create URL(...) is deprecated since Java 20
 
+/**
+ * Creates a [URI] object from the given URI string.
+ *
+ * @param uri The URI string to create the [URI] object from.
+ * @return A [URI] object representing the given URI string.
+ * @throws URISyntaxException If the given URI string violates syntax rules.
+ */
+@Throws(URISyntaxException::class)
 fun uri(@Language("uri") uri: String) = URI(uri)
