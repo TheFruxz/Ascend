9a294fde9a734827ee4f67a00bf902fdad7808c9
Fruxz <cedricspitzer@outlook.de>
2023-07-05 23:50:06 +0200

    Fix typo in Duration.kt comment
    Corrected a grammatical error in the comment of src/main/kotlin/dev/fruxz/ascend/extension/time/Duration.kt file. Changed "represents" to "represent" to improve the readability of the comment explaining the purpose of a method dividing the duration by 50.

--- src/main/kotlin/dev/fruxz/ascend/extension/time/Duration.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/time/Duration.kt
@@ -4,7 +4,7 @@ import kotlin.time.Duration
 
 /**
  * Takes the [Duration.inWholeMilliseconds] and divides it by 50,
- * to represents the duration in minecraft-ticks (20 ticks in a second).
+ * to represent the duration in minecraft-ticks (20 ticks in a second).
  * @author Fruxz
  * @since 1.0
  */
