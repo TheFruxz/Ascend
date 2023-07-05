c46ddf9e7ed98b6fce66837d9951e9c62c1c3ae8
Fruxz <cedricspitzer@outlook.de>
2023-07-05 23:48:52 +0200

    Add detailed function comments in the Hashing module
    This commit includes the addition of distinct and apt comments for the hash functions in the Hashing.kt file. These comments contain the purpose of each function and the parameters involved which serves to improve code readability and understanding. The function comments were added for md5, sha256, sha512, and sha1. Additionally, the comments in the 'hashed' function have been updated for clarity.

--- src/main/kotlin/dev/fruxz/ascend/extension/security/Hashing.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/security/Hashing.kt
@@ -1,16 +1,41 @@
 package dev.fruxz.ascend.extension.security
 
+/**
+ * Computes the MD5 hash value of the given string.
+ *
+ * @param input The input string for which the MD5 hash value is to be calculated.
+ * @return The MD5 hash value of the input string as a hexadecimal string.
+ */
 fun md5(input: String) = HashUtils.md5(input)
 
+/**
+ * Computes the SHA-512 hash value of the given input string.
+ *
+ * @param input the string to compute the hash value for
+ * @return the SHA-512 hash value represented as a string
+ */
 fun sha512(input: String) = HashUtils.sha512(input)
 
+/**
+ * Computes the SHA-256 hash value of the given input string.
+ *
+ * @param input The input string to compute the hash value for.
+ * @return The SHA-256 hash value as a string.
+ */
 fun sha256(input: String) = HashUtils.sha256(input)
 
+/**
+ * Computes the SHA-1 hash value of the given input string.
+ *
+ * @param input The input string for which the SHA-1 hash value needs to be calculated.
+ * @return The SHA-1 hash value of the input string.
+ */
 fun sha1(input: String) = HashUtils.sha1(input)
 
 /**
- * This function hashes this [String] with the given [HashType].
- * @author Fruxz
- * @since 1.0
+ * This function hashes a given string with the specified hash algorithm.
+ *
+ * @param type The hash algorithm to use.
+ * @return The hashed representation of the string.
  */
 infix fun String.hashed(type: HashType) = type.hash(this)
