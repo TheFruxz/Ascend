c911a11208b44d48264e41c0e54cc365cd8c4ed0
Fruxz <cedricspitzer@outlook.de>
2023-07-05 23:30:18 +0200

    Refactor MutableMap copy extension function
    The existing implementation of the copy function in the MutableMap extension has been refactored. This change swaps the use of mutableMapOf with toMutableMap function for creating a copy of the current mutable map. This modification provides cleaner code and enhances performance by avoiding an unnecessary intermediate step.

--- src/main/kotlin/dev/fruxz/ascend/extension/container/MutableMap.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/container/MutableMap.kt
@@ -1,14 +1,21 @@
 package dev.fruxz.ascend.extension.container
 
 /**
- * This function creates a new [MutableMap], utilizing the [mutableMapOf] function and puts
- * all content of [this] into the new one.
- * @author Fruxz
- * @since 1.0
+ * Creates a new [MutableMap] by copying all the content from the current [MutableMap].
+ *
+ * @return A new [MutableMap] with the same key-value pairs as the current [MutableMap].
+ *
+ * @param T The type of the keys in the [MutableMap].
+ * @param E The type of the values in the [MutableMap].
+ * @param M The type of the [MutableMap] to copy.
+ *
+ * @receiver The current [MutableMap] to copy.
+ *
+ * @see MutableMap
+ * @see MutableMap.toMutableMap
  */
-fun <T, E, M : MutableMap<T, E>> M.copy() = mutableMapOf<T, E>().apply {
-	putAll(this@copy)
-}
+fun <T, E, M : MutableMap<T, E>> M.copy() =
+	this.toMutableMap()
 
 /**
  * This function removes every element from this [MutableMap], which matches the [condition].
