12a18f385a8a316a02c713283276d5db3992dc35
TheFruxz <cedricspitzer@outlook.de>
2023-07-07 23:43:13 +0200

    🔧 URL.copyTo(...) now automatically creates parent

--- src/main/kotlin/dev/fruxz/ascend/extension/network/URL.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/network/URL.kt
@@ -7,17 +7,21 @@ import java.nio.file.CopyOption
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
+import kotlin.io.path.createParentDirectories
 
 /**
- * This function downloads the content of the given [URL] to the given [destination] path.
+ * Downloads the content of the given URL to the specified destination path.
+ * If the parent directories of the destination path do not exist, they will be created.
+ *
  * @param destination The destination path to download the content to.
- * @param options The [CopyOption]s to use when copying the content.
+ * @param options The options to use when copying the content.
  * @return The number of bytes that have been copied.
  * @author Fruxz
  * @since 2023.1
  */
 suspend inline fun URL.copyTo(destination: Path, vararg options: CopyOption) =
 	withContext(Dispatchers.IO) {
+		destination.createParentDirectories()
 		Files.copy(openStream(), destination, *options)
 	}
 
