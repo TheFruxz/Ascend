57b93af007a5ac3d790f5b13dbba02d1446e8498
TheFruxz <cedricspitzer@outlook.de>
2023-07-07 23:26:40 +0200

    🔧 Long.outOf(...) is now marked as RefactoringCandidate

--- src/main/kotlin/dev/fruxz/ascend/extension/math/BigPercentage.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/math/BigPercentage.kt
@@ -1,10 +1,12 @@
 package dev.fruxz.ascend.extension.math
 
 import dev.fruxz.ascend.annotation.ExperimentalAscendApi
+import dev.fruxz.ascend.annotation.RefactoringCandidate
 
 @Suppress("DeprecatedCallableAddReplaceWith")
 @Deprecated(message = "The name of this function may change in the future!")
 @ExperimentalAscendApi
+@RefactoringCandidate(since = "2023.3")
 infix fun Long.outOf(denominator: Long) = intPercentageOf(this, denominator)
 
 fun intPercentageOf(numerator: Long, denominator: Long) = (numerator * 100.0 / denominator + .5).toInt()
