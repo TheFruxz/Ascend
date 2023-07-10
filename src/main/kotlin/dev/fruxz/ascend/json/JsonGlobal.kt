3becf41f65ba70ac95d4089ddf7af0c2360b641a
TheFruxz <cedricspitzer@outlook.de>
2023-07-10 23:21:51 +0200

    🔧 Reworked the globalJson architecture
    With an new way work with the json architecture with an way smaller footprint. Also moved the code from global scope to an object.

--- src/main/kotlin/dev/fruxz/ascend/json/JsonGlobal.kt
+++ src/main/kotlin/dev/fruxz/ascend/json/JsonGlobal.kt
@@ -1,120 +1,15 @@
 package dev.fruxz.ascend.json
 
-import dev.fruxz.ascend.extension.dump
-import dev.fruxz.ascend.extension.forceCast
-import dev.fruxz.ascend.json.serializer.AdaptiveSerializer
-import dev.fruxz.ascend.json.serializer.ColorSerializer
-import kotlinx.serialization.KSerializer
+import dev.fruxz.ascend.tool.JsonManager
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonBuilder
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.SerializersModuleBuilder
-import java.awt.Color
-import kotlin.reflect.KClass
-
-// TODO create own manager for this, instead of writing global variables
-
-internal val runningJsonModuleModifications = mutableListOf<SerializersModuleBuilder.() -> Unit>({
-    contextual(Any::class, AdaptiveSerializer())
-    contextual(Color::class, ColorSerializer())
-})
-internal var lastKnownJsonModuleModifications = listOf<SerializersModuleBuilder.() -> Unit>()
-
-internal val runningJsonModifications = mutableListOf<JsonBuilder.() -> Unit>()
-internal var lastKnownJsonModifications = listOf<JsonBuilder.() -> Unit>()
-
-internal var contextuals = setOf<SerializersModule>()
-internal var contextualUpdate = false
-
-private fun produceGlobalJson() = Json {
-    prettyPrint = true
-    isLenient = true
-    ignoreUnknownKeys = true
-    coerceInputValues = true
-    encodeDefaults = true
-    explicitNulls = true
-    allowStructuredMapKeys = true
-    allowSpecialFloatingPointValues = true
-
-    serializersModule = SerializersModule {
-        include(serializersModule)
-
-        contextuals.forEach { contextual ->
-            include(contextual)
-        }
-        runningJsonModuleModifications.forEach {
-            this.apply(it)
-        }
-    }
-
-    runningJsonModifications.forEach {
-        this.apply(it)
-    }
-
-}
 
 /**
- * The current cached state of the json base,
- * can change, if modification-list updates!
- * @see globalJson
- * @author Fruxz
- * @since 2023.1
- */
-private var globalJsonCache: Json? = null
-
-/**
- * This is the base json, which Ascend provides for you.
+ * This is the base json, which Ascend provides.
  * This value returns the current [Json] from the cached value,
  * or creates a new one, if no Json exists, or its modifications
  * are outdated.
+ * @see JsonManager
  * @author Fruxz
  * @since 2023.1
  */
-var globalJson: Json
-    get() {
-        if (globalJsonCache == null
-            || lastKnownJsonModuleModifications != runningJsonModuleModifications
-            || lastKnownJsonModifications != runningJsonModifications
-            || contextualUpdate
-        ) {
-            contextualUpdate = false
-
-            produceGlobalJson().let {
-                globalJsonCache = it
-                return it
-            }
-
-        } else
-            return globalJsonCache!!
-    }
-    set(value) { globalJsonCache = value }
-
-/**
- * Adds a custom modification to the [SerializersModule] during its build process
- * for the [Json] object, used at the toJson and fromJson functions.
- * @param process the modification to the [SerializersModuleBuilder] in the building process
- * @author Fruxz
- * @since 2023.1
- */
-fun appendGlobalJsonModuleModification(process: SerializersModuleBuilder.() -> Unit) {
-    runningJsonModuleModifications += process
-}
-
-fun <T : Any> appendGlobalJsonContextual(clazz: KClass<T>, serializer: KSerializer<out T>) {
-    contextuals += SerializersModule {
-        contextual(clazz, serializer = serializer.forceCast())
-    }
-    contextualUpdate = true
-    globalJson.dump() // trigger update of module
-}
-
-/**
- * Adds a custom modification to the [Json] during its build process
- * for the [globalJson], used at the toJson and fromJson functions.
- * @param process the modification to the [JsonBuilder] in the building process
- * @author Fruxz
- * @since 2023.1
- */
-fun appendGlobalJsonModification(process: JsonBuilder.() -> Unit) {
-    runningJsonModifications += process
-}
+var globalJson: Json = JsonManager.json
---
+++ src/main/kotlin/dev/fruxz/ascend/tool/JsonManager.kt
@@ -0,0 +1,147 @@
+package dev.fruxz.ascend.tool
+
+import dev.fruxz.ascend.extension.dump
+import dev.fruxz.ascend.extension.forceCast
+import dev.fruxz.ascend.json.serializer.AdaptiveSerializer
+import dev.fruxz.ascend.json.serializer.ColorSerializer
+import dev.fruxz.ascend.tool.smart.Modification
+import dev.fruxz.ascend.tool.smart.modification
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonBuilder
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.SerializersModuleBuilder
+import java.awt.Color
+import kotlin.reflect.KClass
+import dev.fruxz.ascend.json.globalJson
+
+/**
+ * This object manages a json instance, which can be used for every purpose.
+ * It is designed to adapt to new changes and the current environment, but also
+ * being highly customizable.
+ * It automatically updates the json instance if something has changed.
+ * @see globalJson is an easy way to access the current json instance from anywhere.
+ * @author Fruxz
+ * @since 2023.3
+ */
+object JsonManager {
+
+    private val runningJsonModuleModifications = mutableListOf<Modification<SerializersModuleBuilder>>(modification {
+        contextual(Any::class, AdaptiveSerializer())
+        contextual(Color::class, ColorSerializer())
+    })
+    private var moduleModificationsHash: String = runningJsonModuleModifications.hashCode().toString(16)
+
+    private val runningJsonModifications = mutableListOf<Modification<JsonBuilder>>()
+    private var jsonModificationsHash: String = runningJsonModifications.hashCode().toString(16)
+
+    private var contextuals = setOf<SerializersModule>()
+    private var contextualUpdate = false
+
+    /**
+     * Updates the module modifications hash, JSON modifications hash, and contextual update flag.
+     * This function should be called after any modification to the JSON configuration has been made.
+     * @author Fruxz
+     * @since 2023.3
+     */
+    private fun pushUpdate() {
+        moduleModificationsHash = runningJsonModuleModifications.hashCode().toString(16)
+        jsonModificationsHash = runningJsonModifications.hashCode().toString(16)
+        contextualUpdate = false
+    }
+
+    private fun computeJson() = Json {
+        prettyPrint = true
+        isLenient = true
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+        encodeDefaults = true
+        explicitNulls = true
+        allowStructuredMapKeys = true
+        allowSpecialFloatingPointValues = true
+
+        serializersModule = SerializersModule {
+            include(serializersModule)
+
+            contextuals.forEach { contextual ->
+                include(contextual)
+            }
+            runningJsonModuleModifications.forEach {
+                this.apply(it::modify)
+            }
+        }
+
+        runningJsonModifications.forEach {
+            this.apply(it::modify)
+        }
+
+    }
+
+    /**
+     * The current cached state of the json base,
+     * can change, if modification-list updates!
+     * @see json
+     * @author Fruxz
+     * @since 2023.1
+     */
+    private var cachedJson: Json? = null
+
+    /**
+     * This is the base json, which Ascend provides for you.
+     * This value returns the current [Json] from the cached value,
+     * or creates a new one, if no Json exists, or its modifications
+     * are outdated.
+     * @author Fruxz
+     * @since 2023.1
+     */
+    var json: Json
+        get() {
+            if (cachedJson == null
+                || moduleModificationsHash != runningJsonModuleModifications.hashCode().toString(16)
+                || jsonModificationsHash != runningJsonModifications.hashCode().toString(16)
+                || contextualUpdate
+            ) {
+
+                pushUpdate()
+
+                computeJson().let {
+                    cachedJson = it
+                    return it
+                }
+
+            } else
+                return cachedJson!!
+        }
+        set(value) { cachedJson = value }
+
+    /**
+     * Adds a custom modification to the [SerializersModule] during its build process
+     * for the [Json] object, used at the toJson and fromJson functions.
+     * @param process the modification to the [SerializersModuleBuilder] in the building process
+     * @author Fruxz
+     * @since 2023.1
+     */
+    fun addModuleModification(process: Modification<SerializersModuleBuilder>) {
+        runningJsonModuleModifications += process
+    }
+
+    fun <T : Any> addContextual(clazz: KClass<T>, serializer: KSerializer<out T>) {
+        contextuals += SerializersModule {
+            contextual(clazz, serializer = serializer.forceCast())
+        }
+        contextualUpdate = true
+        json.dump() // trigger update of module
+    }
+
+    /**
+     * Adds a custom modification to the [Json] during its build process
+     * for the [json], used at the toJson and fromJson functions.
+     * @param process the modification to the [JsonBuilder] in the building process
+     * @author Fruxz
+     * @since 2023.1
+     */
+    fun addJsonModification(process: Modification<JsonBuilder>) {
+        runningJsonModifications += process
+    }
+
+}
