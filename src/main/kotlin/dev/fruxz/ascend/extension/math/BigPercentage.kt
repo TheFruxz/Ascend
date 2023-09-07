d959bb4df6a5f1dd70815677056059491dbb80ce
Fruxz <cedricspitzer@outlook.de>
2023-09-07 13:44:30 +0200

    chore: update name from outOf to intPercentageOf

--- src/main/kotlin/dev/fruxz/ascend/extension/math/BigPercentage.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/math/BigPercentage.kt
@@ -3,10 +3,8 @@ package dev.fruxz.ascend.extension.math
 import dev.fruxz.ascend.annotation.ExperimentalAscendApi
 import dev.fruxz.ascend.annotation.RefactoringCandidate
 
-@Suppress("DeprecatedCallableAddReplaceWith")
-@Deprecated(message = "The name of this function may change in the future!")
 @ExperimentalAscendApi
-@RefactoringCandidate(since = "2023.3")
-infix fun Long.outOf(denominator: Long) = intPercentageOf(this, denominator)
+infix fun Long.intPercentageOf(denominator: Long) = intPercentageOf(this, denominator)
 
+@ExperimentalAscendApi
 fun intPercentageOf(numerator: Long, denominator: Long) = (numerator * 100.0 / denominator + .5).toInt()
