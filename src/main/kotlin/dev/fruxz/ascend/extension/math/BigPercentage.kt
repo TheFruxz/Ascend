744913bc14acfba5882000249685e43e5ff147dc
TheFruxz <cedricspitzer@outlook.de>
2023-09-11 16:16:26 +0200

    fix: remove jvm clashing function intPercentageOf
    because it was extending Long, but global intPercentageOf is already an existing function

--- src/main/kotlin/dev/fruxz/ascend/extension/math/BigPercentage.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/math/BigPercentage.kt
@@ -3,7 +3,4 @@ package dev.fruxz.ascend.extension.math
 import dev.fruxz.ascend.annotation.ExperimentalAscendApi
 
 @ExperimentalAscendApi
-infix fun Long.intPercentageOf(denominator: Long) = intPercentageOf(this, denominator)
-
-@ExperimentalAscendApi
 fun intPercentageOf(numerator: Long, denominator: Long) = (numerator * 100.0 / denominator + .5).toInt()
