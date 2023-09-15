49bfa2871ae1d31939dc83fb37863adcacfcfea9
TheFruxz <cedricspitzer@outlook.de>
2023-09-15 07:36:38 +0200

    chore: add kdoc to ExperimentalAscendApi annotation

--- src/main/kotlin/dev/fruxz/ascend/annotation/ExperimentalAscendApi.kt
+++ src/main/kotlin/dev/fruxz/ascend/annotation/ExperimentalAscendApi.kt
@@ -1,5 +1,14 @@
 package dev.fruxz.ascend.annotation
 
+/**
+ * Marks the annotated element as part of the experimental Ascend API.
+ *
+ * This annotation should be used to indicate that the annotated element is experimental
+ * and may undergo changes or be removed in future versions. It serves as a warning for
+ * developers to use the element with caution.
+ * @author Fruxz
+ * @since 2023.1
+ **/
 @MustBeDocumented
 @RequiresOptIn("This is a experimental feature, proceed with caution!")
 internal annotation class ExperimentalAscendApi
