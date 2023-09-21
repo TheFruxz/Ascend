594a6e979e6c00d42c7c3cb979db68dbca40abc6
Fruxz <cedricspitzer@outlook.de>
2023-09-21 11:20:11 +0200

    chore: update kotlin to 1.9.20-Beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.20-Beta"
+    kotlin("jvm") version "1.9.20-Beta2"
     kotlin("plugin.serialization") version "1.9.10"
     id("org.jetbrains.dokka") version "1.9.0"
     `maven-publish`
