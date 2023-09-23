5c31baea43fd818fae0f84b9de8af165662c82d1
Fruxz <cedricspitzer@outlook.de>
2023-09-23 20:21:30 +0200

    fix: array creation no longer creates two arrays

--- src/main/kotlin/dev/fruxz/ascend/extension/container/Array.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/container/Array.kt
@@ -3,25 +3,25 @@ package dev.fruxz.ascend.extension.container
 import dev.fruxz.ascend.annotation.LanguageFeature
 
 @LanguageFeature
-operator fun Int.Companion.get(vararg int: Int) = arrayOf(*int.toTypedArray())
+operator fun Int.Companion.get(vararg int: Int): Array<Int> = int.toTypedArray()
 
 @LanguageFeature
-operator fun Long.Companion.get(vararg long: Long) = arrayOf(*long.toTypedArray())
+operator fun Long.Companion.get(vararg long: Long): Array<Long> = long.toTypedArray()
 
 @LanguageFeature
-operator fun Float.Companion.get(vararg float: Float) = arrayOf(*float.toTypedArray())
+operator fun Float.Companion.get(vararg float: Float): Array<Float> = float.toTypedArray()
 
 @LanguageFeature
-operator fun Double.Companion.get(vararg double: Double) = arrayOf(*double.toTypedArray())
+operator fun Double.Companion.get(vararg double: Double): Array<Double> = double.toTypedArray()
 
 @LanguageFeature
-operator fun Boolean.Companion.get(vararg boolean: Boolean) = arrayOf(*boolean.toTypedArray())
+operator fun Boolean.Companion.get(vararg boolean: Boolean): Array<Boolean> = boolean.toTypedArray()
 
 @LanguageFeature
-operator fun Char.Companion.get(vararg char: Char) = arrayOf(*char.toTypedArray())
+operator fun Char.Companion.get(vararg char: Char): Array<Char> = char.toTypedArray()
 
 @LanguageFeature
-operator fun Byte.Companion.get(vararg byte: Byte) = arrayOf(*byte.toTypedArray())
+operator fun Byte.Companion.get(vararg byte: Byte): Array<Byte> = byte.toTypedArray()
 
 @LanguageFeature
-operator fun Short.Companion.get(vararg short: Short) = arrayOf(*short.toTypedArray())
+operator fun Short.Companion.get(vararg short: Short): Array<Short> = short.toTypedArray()
