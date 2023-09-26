02f0b9383a237a2614c4f80189865a60d4653056
TheFruxz <cedricspitzer@outlook.de>
2023-09-27 00:07:11 +0200

    chore: add warning about jsonGlobal property redirect

--- src/main/kotlin/dev/fruxz/ascend/json/JsonGlobal.kt
+++ src/main/kotlin/dev/fruxz/ascend/json/JsonGlobal.kt
@@ -8,6 +8,7 @@ import kotlinx.serialization.json.Json
  * This value returns the current [Json] from the cached value,
  * or creates a new one, if no Json exists, or its modifications
  * are outdated.
+ * ***Attention. Changing this var, changes [JsonManager.json]!***
  * @see JsonManager
  * @author Fruxz
  * @since 2023.1
