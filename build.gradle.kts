0108f95faa457fb9d32e82f17e33bcdb646c4c27
TheFruxz <cedricspitzer@outlook.de>
2023-09-28 16:01:49 +0200

    chore: update jetbrains exposed to 0.44.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
-    implementation("org.jetbrains.exposed:exposed-core:0.43.0")
-    implementation("org.jetbrains.exposed:exposed-dao:0.43.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.43.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.44.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.44.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.44.0")
 
 }
 
