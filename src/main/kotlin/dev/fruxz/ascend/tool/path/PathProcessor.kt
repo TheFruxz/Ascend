05226e5b3506933b7b5585c806da8c194ee45797
Fruxz <cedricspitzer@outlook.de>
2023-10-17 22:47:46 +0200

    chore: deprecated path protocols/processors
    in favor of built-in solution, which should be more seamless

--- src/main/kotlin/dev/fruxz/ascend/tool/path/PathProcessor.kt
+++ src/main/kotlin/dev/fruxz/ascend/tool/path/PathProcessor.kt
@@ -1,10 +1,9 @@
 package dev.fruxz.ascend.tool.path
 
-import dev.fruxz.ascend.annotation.RefactoringCandidate
 import java.io.File
 import java.nio.file.Path
 
-@RefactoringCandidate
+@Deprecated(message = "Use built-in path protocols instead.")
 object PathProcessor {
 
     val protocols: MutableMap<String, PathProtocol> = mutableMapOf()
--- src/main/kotlin/dev/fruxz/ascend/tool/path/PathProtocol.kt
+++ src/main/kotlin/dev/fruxz/ascend/tool/path/PathProtocol.kt
@@ -1,10 +1,9 @@
 package dev.fruxz.ascend.tool.path
 
-import dev.fruxz.ascend.annotation.RefactoringCandidate
 import java.io.File
 import java.nio.file.Path
 
-@RefactoringCandidate
+@Deprecated(message = "Use built-in path protocols instead.")
 fun interface PathProtocol {
 
     fun processor(path: Path): File?
