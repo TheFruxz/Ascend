f14457146b3c0e64d172842b4bedb9aff2dd5b33
Fruxz <cedricspitzer@outlook.de>
2023-11-07 15:41:41 +0100

    feature: reintroduce identity

---
+++ src/main/kotlin/dev/fruxz/ascend/tool/smart/identity/Identity.kt
@@ -0,0 +1,9 @@
+package dev.fruxz.ascend.tool.smart.identity
+
+/**
+ * A value class to mark something an identity of something identifiable
+ * @author Fruxz
+ * @since 2023.5
+ */
+@JvmInline
+value class Identity<T>(val value: T)
