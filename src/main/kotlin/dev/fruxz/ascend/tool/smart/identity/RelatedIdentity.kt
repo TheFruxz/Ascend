6b17a9129f4d06bdbd8f1d97aa9ca7c6acdd1b2f
Fruxz <cedricspitzer@outlook.de>
2023-11-07 15:44:39 +0100

    feature: add related identity

--- src/main/kotlin/dev/fruxz/ascend/tool/smart/identity/RelatedIdentity.kt
+++ src/main/kotlin/dev/fruxz/ascend/tool/smart/identity/RelatedIdentity.kt
@@ -6,11 +6,13 @@ package dev.fruxz.ascend.tool.smart.identity
  * @since 2023.5
  */
 @JvmInline
-value class Identity<T>(val value: T) {
+value class RelatedIdentity<O, T>(val value: T) {
+
+    fun <E> ofType() = RelatedIdentity<E, T>(value = value)
 
     companion object {
 
-        fun <T> of(value: T) = Identity(value = value)
+        fun <O, T> of(value: T) = RelatedIdentity<O, T>(value = value)
 
     }
 
