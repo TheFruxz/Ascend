35fc8e9d11965033a7d396ec4e98b93227dfff5f
TheFruxz <cedricspitzer@outlook.de>
2023-12-17 21:29:40 +0100

    chore: update kotlin to 2.0.0-Beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.0-Beta1"
+    kotlin("jvm") version "2.0.0-Beta2"
     kotlin("plugin.serialization") version "1.9.21"
     id("org.jetbrains.dokka") version "1.9.10"
     `maven-publish`
