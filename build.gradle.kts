8f29bde07f936cfe94f31a765d3be638ff650052
TheFruxz <cedricspitzer@outlook.de>
2023-12-22 00:34:42 +0100

    chore: update kotlin to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.21"
     id("org.jetbrains.dokka") version "1.9.10"
     `maven-publish`
