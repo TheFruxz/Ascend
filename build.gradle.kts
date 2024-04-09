653f6f09884543c7b5d301f41e547a1824c62714
TheFruxz <cedricspitzer@outlook.de>
2024-04-09 15:07:53 +0200

    chore: update kotlin to 2.0.0-RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.0-Beta3"
+    kotlin("jvm") version "2.0.0-RC1"
     kotlin("plugin.serialization") version "1.9.21"
     id("org.jetbrains.dokka") version "1.9.10"
     `maven-publish`
