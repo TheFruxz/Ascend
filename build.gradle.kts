607585c10891701975e995ef3e54f34614ab4b73
TheFruxz <cedricspitzer@outlook.de>
2024-05-24 14:51:26 +0200

    chore: temporarily disable kdoc tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,23 +34,6 @@ dependencies {
 
 }
 
-val dokkaHtmlJar by tasks.register<Jar>("dokkaHtmlJar") {
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
-    archiveClassifier.set("html-docs")
-}
-
-val dokkaJavadocJar by tasks.register<Jar>("dokkaJavadocJar") {
-    dependsOn(tasks.dokkaJavadoc)
-    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
-    archiveClassifier.set("javadoc")
-}
-
-val sourceJar by tasks.register<Jar>("sourceJar") {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
-}
-
 publishing {
 
     repositories {
@@ -67,12 +50,6 @@ publishing {
     publications.create("Ascend", MavenPublication::class) {
         artifactId = name.lowercase()
         version = version.lowercase()
-
-        artifact(dokkaJavadocJar)
-        artifact(dokkaHtmlJar)
-        artifact(sourceJar) {
-            classifier = "sources"
-        }
 
         from(components["kotlin"])
 
