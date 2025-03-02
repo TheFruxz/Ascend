2206a04d86f1722fb9c2f7b5a2aa0c818cdc04ef
Fruxz <cedricspitzer@outlook.de>
2025-03-02 19:25:57 +0100

    chore: deprecate createMissingTablesAndColumns functions

--- src/main/kotlin/dev/fruxz/ascend/extension/exposed/Table.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/exposed/Table.kt
@@ -9,6 +9,8 @@ import org.jetbrains.exposed.sql.Table
  * @author Fruxz
  * @since 2023.1
  */
+@Suppress("DEPRECATION")
+@Deprecated("Deprecated by JetBrains Exposed, see SchemaUtils.createMissingTablesAndColumns")
 fun Table.initialize(withLogs: Boolean = true) =
 	SchemaUtils.createMissingTablesAndColumns(this, withLogs = withLogs)
 
@@ -18,5 +20,7 @@ fun Table.initialize(withLogs: Boolean = true) =
  * @author Fruxz
  * @since 2023.1
  */
+@Suppress("DEPRECATION")
+@Deprecated("Deprecated by JetBrains Exposed, see SchemaUtils.createMissingTablesAndColumns")
 fun initializeTables(vararg tables: Table, withLogs: Boolean = true) =
 	SchemaUtils.createMissingTablesAndColumns(*tables, withLogs = withLogs)
