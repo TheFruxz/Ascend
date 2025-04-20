28c71dfa2564a66caa0d10cabc67b2328da022e7
Fruxz <cedricspitzer@outlook.de>
2025-04-20 17:59:40 +0200

    feature: enhanced JsonIO architecture
    instead of relying on string based backend, the json IO functions now utilize streams instead, for better performance and big file size support, both for path/file read and write!

--- src/main/kotlin/dev/fruxz/ascend/json/JsonIO.kt
+++ src/main/kotlin/dev/fruxz/ascend/json/JsonIO.kt
@@ -1,152 +1,184 @@
 package dev.fruxz.ascend.json
 
 import dev.fruxz.ascend.extension.readTextOrNull
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import java.io.File
+import java.io.IOException
 import java.nio.charset.Charset
 import java.nio.file.OpenOption
 import java.nio.file.Path
+import kotlin.io.inputStream
 import kotlin.io.path.*
+import kotlin.jvm.Throws
 
 /**
  * This function writes the given [this] object to a JSON file via the [globalJson]
- * and [toJsonString] function from the Kotlinx serialization library.
+ * and [toJsonStream] function from the Kotlinx serialization library.
  * This process can throw exceptions if something goes wrong!
- * @see toJsonString
- * @see writeText
+ * @see toJsonStream
+ * @see outputStream
  * @return The path itself.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.writeJson(content: T, charset: Charset = Charsets.UTF_8, json: Json = globalJson, vararg options: OpenOption) = apply { writeText(content.toJsonString(json = json), charset, *options) }
+@Throws(SerializationException::class, IOException::class)
+inline fun <reified T> Path.writeJson(
+    content: T,
+    json: Json = globalJson,
+    vararg options: OpenOption
+) = apply {
+    content.toJsonStream(
+        stream = this.outputStream(options = options),
+        json = json
+    )
+}
 
 /**
  * This function writes the given [this] object to a JSON file via the [globalJson]
- * and [toJsonString] function from the Kotlinx serialization library.
+ * and [toJsonStream] function from the Kotlinx serialization library.
  * This process can throw exceptions if something goes wrong!
- * @see toJsonString
- * @see writeText
+ * @see toJsonStream
  * @return The file itself.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> File.writeJson(content: T, charset: Charset = Charsets.UTF_8, json: Json = globalJson) = apply { writeText(content.toJsonString(json = json), charset) }
+@Throws(SerializationException::class, IOException::class)
+inline fun <reified T> File.writeJson(
+    content: T,
+    json: Json = globalJson
+) = apply {
+    content.toJsonStream(
+        stream = this.outputStream(),
+        json = json
+    )
+}
 
 /**
  * This function writes the given [content], to the file under [this] path, via the [writeJson]
  * if the file under [this] path currently does not exist.
  * @param content the content to write as json to the file.
- * @param createParent if the parent directories will be created during to write
- * @param charset the charset to use for to write
+ * @param createParent if the parent directories will be created before writing
  * @see writeJson
  * @return the path itself.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.writeJsonIfNotExists(content: T, createParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson, vararg options: OpenOption) = apply {
-    if (!exists()) {
-        if (createParent) parent.createDirectories()
-        writeJson(content, charset, json = json, *options)
-    }
+@Throws(SerializationException::class, IOException::class, SecurityException::class)
+
+inline fun <reified T> Path.writeJsonIfNotExists(content: T, createParent: Boolean = true, json: Json = globalJson, vararg options: OpenOption) = apply {
+    if (exists()) return@apply
+
+    if (createParent) parent.createDirectories()
+    writeJson(content, json = json, *options)
 }
 
 /**
  * This function writes the given [content], to [this] file , via the [writeJson]
  * if the file currently does not exist.
  * @param content the content to write as json to the file
- * @param createParent if the parent directories will be created during the write
- * @param charset the charset to use fo r to write
+ * @param createParent if the parent directories will be created before writing
  * @see writeJson
  * @return the file itself
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> File.writeJsonIfNotExists(content: T, createParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson) = apply {
-    if (!exists()) {
-        if (createParent) parentFile.mkdirs()
-        writeJson(content, charset, json = json)
-    }
+@Throws(SerializationException::class, IOException::class, SecurityException::class)
+inline fun <reified T> File.writeJsonIfNotExists(content: T, createParent: Boolean = true, json: Json = globalJson) = apply {
+    if (exists()) return@apply
+
+    if (createParent) parentFile.mkdirs()
+    writeJson(content, json = json)
 }
 
 /**
  * This function writes the given [content], to the file under [this] path, via the [writeJson]
  * if the file under [this] path currently is empty or does not exist.
  * @param content the content to write as json to the file.
- * @param createParent if the parent directories will be created during to write
- * @param charset the charset to use for to write
+ * @param createParent if the parent directories will be created before writing
  * @see writeJson
  * @return the path itself.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.writeJsonIfEmpty(content: T, createParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson, vararg options: OpenOption) = apply {
-    if (readTextOrNull(charset)?.isBlank() != false) {
-        if (createParent) parent.createDirectories()
-        writeJson(content, charset, json = json, *options)
-    }
+inline fun <reified T> Path.writeJsonIfEmpty(content: T, createParent: Boolean = true, json: Json = globalJson, vararg options: OpenOption) = apply {
+    if (this.fileSize() > 0L) return@apply
+
+    if (createParent) parent.createDirectories()
+    writeJson(content, json = json, *options)
 }
 
 /**
  * This function writes the given [content], to [this] file, via the [writeJson]
  * if [this] file is currently empty or does not exist.
  * @param content the content to write as json to the file.
- * @param createParent if the parent directories will be created during to write
- * @param charset the charset to use for to write
+ * @param createParent if the parent directories will be created before writing
  * @see writeJson
  * @return the file itself.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> File.writeJsonIfEmpty(content: T, createParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson) = apply {
-    if (readTextOrNull(charset)?.isBlank() != false) {
-        if (createParent) parentFile.mkdirs()
-        writeJson(content, charset, json = json)
-    }
+inline fun <reified T> File.writeJsonIfEmpty(content: T, createParent: Boolean = true, json: Json = globalJson) = apply {
+    if (this.length() > 0L) return@apply
+
+    if (createParent) parentFile.mkdirs()
+    writeJson(content, json = json)
 }
 
 /**
  * This function writes the given [content], to the file under [this] path, via the [writeJson]
  * if the file under [this] path is currently blank or does not exist.
  * @param content the content to write as json to the file.
- * @param createParent if the parent directories will be created during to write
- * @param charset the charset to use for to write
+ * @param createParent if the parent directories will be created before writing
  * @see writeJson
  * @return the path itself
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.writeJsonIfBlank(content: T, createParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson, vararg options: OpenOption) = apply {
-    if (readTextOrNull(charset)?.isBlank() != false) {
-        if (createParent) parent.createDirectories()
-        writeJson(content, charset, json = json, *options)
-    }
+inline fun <reified T> Path.writeJsonIfBlank(content: T, createParent: Boolean = true, json: Json = globalJson, vararg options: OpenOption) = apply {
+    if (fileSize() > 0L) return@apply
+    if (bufferedReader().useLines { it.any { it.isNotBlank() } }) return@apply
+
+    if (createParent) parent.createDirectories()
+    writeJson(content, json = json, *options)
 }
 
-inline fun <reified T> File.writeJsonIfBlank(content: T, createParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson) = apply {
-    if (readTextOrNull(charset)?.isBlank() != false) {
-        if (createParent) parentFile.mkdirs()
-        writeJson(content, charset, json = json)
-    }
+/**
+ * This function writes the given [content], to the file under [this] file, via the [writeJson]
+ * if the file under [this] path is currently blank or does not exist.
+ * @param content the content to write as json to the file.
+ * @param createParent if the parent directories will be created before writing
+ * @see writeJson
+ * @return the path itself
+ * @author Fruxz
+ * @since 2023.1
+ */
+inline fun <reified T> File.writeJsonIfBlank(content: T, createParent: Boolean = true, json: Json = globalJson) = apply {
+    if (length() > 0L) return@apply
+    if (bufferedReader().useLines { it.any { it.isNotBlank() } }) return@apply
+
+    if (createParent) parentFile.mkdirs()
+    writeJson(content, json = json)
 }
 
 
 // input json from file
 
 /**
- * This function returns the content of [this] Path using the [readText] and [fromJsonString] function.
+ * This function returns the content of [this] Path using the [inputStream] and [fromJsonStream] function.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.readJson(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readText(charset).fromJsonString<T>(json = json)
+inline fun <reified T> Path.readJson(json: Json = globalJson) = inputStream().fromJsonStream<T>(json = json)
 
 /**
- * This function returns the content of [this] File using the [readTextOrNull] and [fromJsonStringOrNull] function.
+ * This function returns the content of [this] File using the [inputStream] and [fromJsonStreamOrNull] function.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.readJsonOrNull(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readTextOrNull(charset)?.fromJsonStringOrNull<T>(json = json)
+inline fun <reified T> Path.readJsonOrNull(json: Json = globalJson) = inputStream().fromJsonStreamOrNull<T>(json = json)
 
 /**
  * This function reads the file content and parses it to a [JsonElement]
@@ -155,6 +187,7 @@ inline fun <reified T> Path.readJsonOrNull(charset: Charset = Charsets.UTF_8, js
  * @author Fruxz
  * @since 2023.1
  */
+@Deprecated(message = "This function utilizes the readText function, which is not recommended for large files and not used by ascend anymore.")
 fun Path.readJsonElement(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readText(charset).parseToJsonElement(json = json)
 
 /**
@@ -164,6 +197,7 @@ fun Path.readJsonElement(charset: Charset = Charsets.UTF_8, json: Json = globalJ
  * @author Fruxz
  * @since 2023.1
  */
+@Deprecated(message = "This function utilizes the readText function, which is not recommended for large files and not used by ascend anymore.")
 fun Path.readJsonElementOrNull(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readTextOrNull(charset)?.parseToJsonElementOrNull(json = json)
 
 /**
@@ -173,6 +207,7 @@ fun Path.readJsonElementOrNull(charset: Charset = Charsets.UTF_8, json: Json = g
  * @author Fruxz
  * @since 2023.1
  */
+@Deprecated(message = "This function utilizes the readText function, which is not recommended for large files and not used by ascend anymore.")
 fun Path.readJsonObject(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readText(charset).parseToJsonObject(json = json)
 
 /**
@@ -182,21 +217,22 @@ fun Path.readJsonObject(charset: Charset = Charsets.UTF_8, json: Json = globalJs
  * @author Fruxz
  * @since 2023.1
  */
+@Deprecated(message = "This function utilizes the readText function, which is not recommended for large files and not used by ascend anymore.")
 fun Path.readJsonObjectOrNull(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readTextOrNull(charset)?.parseToJsonObjectOrNull(json = json)
 
 /**
- * This function returns the content of [this] File using the [readText] and [fromJsonString] function.
+ * This function returns the content of [this] File using the [inputStream] and [fromJsonStream] function.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> File.readJson(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readText(charset).fromJsonString<T>(json = json)
+inline fun <reified T> File.readJson(json: Json = globalJson) = inputStream().fromJsonStream<T>(json = json)
 
 /**
- * This function returns the content of [this] File using the [readTextOrNull] and [fromJsonStringOrNull] function.
+ * This function returns the content of [this] File using the [inputStream] and [fromJsonStreamOrNull] function.
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> File.readJsonOrNull(charset: Charset = Charsets.UTF_8, json: Json = globalJson) = readTextOrNull(charset)?.fromJsonStringOrNull<T>(json = json)
+inline fun <reified T> File.readJsonOrNull(json: Json = globalJson) = inputStream().fromJsonStreamOrNull<T>(json = json)
 
 // read default
 
@@ -208,21 +244,20 @@ inline fun <reified T> File.readJsonOrNull(charset: Charset = Charsets.UTF_8, js
  * [writeIfBlank] is true, the [default] will be written to the file.
  * @param default the default value to return if the file is blank, non-transformable or does not exist.
  * @param writeIfBlank if the file is blank or does not exist, the [default] will be written to the file.
- * @param writeCreatesParent if the parent directories will be created during the write.
- * @param charset the charset to use for to write
+ * @param writeCreatesParent if the parent directories will be created before writing.
  * @see readJsonOrNull
  * @see Path.createDirectories
  * @see writeJson
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> Path.readJsonOrDefault(default: T, writeIfBlank: Boolean = false, writeCreatesParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson, vararg options: OpenOption) = if (writeIfBlank) {
-    readJsonOrNull<T>(charset) ?: default.also {
+inline fun <reified T> Path.readJsonOrDefault(default: T, writeIfBlank: Boolean = false, writeCreatesParent: Boolean = true, json: Json = globalJson, vararg options: OpenOption) = if (writeIfBlank) {
+    readJsonOrNull<T>() ?: default.also {
         if (writeCreatesParent) parent.createDirectories()
-        writeJson(default, charset, json = json, *options)
+        writeJson(default, json = json, *options)
     }
 } else {
-    readJsonOrNull<T>(charset, json = json) ?: default
+    readJsonOrNull<T>(json = json) ?: default
 }
 
 /**
@@ -233,19 +268,18 @@ inline fun <reified T> Path.readJsonOrDefault(default: T, writeIfBlank: Boolean 
  * [writeIfBlank] is true, the [default] will be written to the file.
  * @param default the default value to return if the file is blank, non-transformable or does not exist.
  * @param writeIfBlank if the file is blank or does not exist, the [default] will be written to the file.
- * @param writeCreatesParent if the parent directories will be created during the write.
- * @param charset the charset to use for to write
+ * @param writeCreatesParent if the parent directories will be created before writing.
  * @see readJsonOrNull
  * @see Path.createDirectories
  * @see writeJson
  * @author Fruxz
  * @since 2023.1
  */
-inline fun <reified T> File.readJsonOrDefault(default: T, writeIfBlank: Boolean = false, writeCreatesParent: Boolean = true, charset: Charset = Charsets.UTF_8, json: Json = globalJson) = if (writeIfBlank) {
-    readJsonOrNull<T>(charset) ?: default.also {
+inline fun <reified T> File.readJsonOrDefault(default: T, writeIfBlank: Boolean = false, writeCreatesParent: Boolean = true, json: Json = globalJson) = if (writeIfBlank) {
+    readJsonOrNull<T>() ?: default.also {
         if (writeCreatesParent) parentFile.mkdirs()
-        writeJson(default, charset, json = json)
+        writeJson(default, json = json)
     }
 } else {
-    readJsonOrNull<T>(charset, json = json) ?: default
+    readJsonOrNull<T>(json = json) ?: default
 }
