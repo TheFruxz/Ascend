98e2e3af48824ea0579a6cabd44ea70b9d37664b
Fruxz <cedricspitzer@outlook.de>
2025-06-09 05:19:42 +0200

    feature: upgraded table initialization
    dropping the need of marked deprecation

--- src/main/kotlin/dev/fruxz/ascend/extension/exposed/Table.kt
+++ src/main/kotlin/dev/fruxz/ascend/extension/exposed/Table.kt
@@ -4,23 +4,19 @@ import org.jetbrains.exposed.v1.core.Table
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 
 /**
- * This function creates or updates [this] table structure in the database.
- * This utilizes the [SchemaUtils.createMissingTablesAndColumns] function.
+ * This function creates [this] table structure in the database.
+ * This utilizes the [SchemaUtils.create] function.
  * @author Fruxz
- * @since 2023.1
+ * @since 2025.6
  */
-@Suppress("DEPRECATION")
-@Deprecated("Deprecated by JetBrains Exposed, see SchemaUtils.createMissingTablesAndColumns")
-fun Table.initialize(withLogs: Boolean = true) =
-	SchemaUtils.createMissingTablesAndColumns(this, withLogs = withLogs)
+fun Table.initialize(inBatch: Boolean = false) =
+	SchemaUtils.create(this, inBatch = inBatch)
 
 /**
- * This function creates or updates the [tables] structure in the database.
- * This utilizes the [SchemaUtils.createMissingTablesAndColumns] function.
+ * This function creates the [tables] structure in the database.
+ * This utilizes the [SchemaUtils.create] function.
  * @author Fruxz
- * @since 2023.1
+ * @since 2025.6
  */
-@Suppress("DEPRECATION")
-@Deprecated("Deprecated by JetBrains Exposed, see SchemaUtils.createMissingTablesAndColumns")
-fun initializeTables(vararg tables: Table, withLogs: Boolean = true) =
-	SchemaUtils.createMissingTablesAndColumns(*tables, withLogs = withLogs)
+fun initializeTables(vararg tables: Table, inBatch: Boolean = false) =
+	SchemaUtils.create(*tables, inBatch = inBatch)
