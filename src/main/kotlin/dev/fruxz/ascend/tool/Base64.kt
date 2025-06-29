5a11ec29ac39c889fcc0c300bb243c885549f2d0
InvalidJoker <0limitdev@gmail.com>
2025-06-30 00:06:13 +0200

    docs: document base64 properly

--- src/main/kotlin/dev/fruxz/ascend/tool/Base64.kt
+++ src/main/kotlin/dev/fruxz/ascend/tool/Base64.kt
@@ -2,19 +2,26 @@ package dev.fruxz.ascend.tool
 
 import java.util.Base64
 
+/**
+ * Utility object for Base64 encoding and decoding.
+ * Provides methods to convert strings to Base64 format and vice versa.
+ *
+ * @author Fruxz
+ * @since 2025.6
+ */
 object Base64 {
 
     /**
      * From String to Base64
      * @author Fruxz
-     * @since 1.0
+     * @since 2025.6
      */
     fun encodeToString(input: String): String = Base64.getEncoder().encodeToString(input.encodeToByteArray())
 
     /**
      * From Base64 to String
      * @author Fruxz
-     * @since 1.0
+     * @since 2025.6
      */
     fun decodeToString(input: String): String = Base64.getDecoder().decode(input.encodeToByteArray()).decodeToString()
 
