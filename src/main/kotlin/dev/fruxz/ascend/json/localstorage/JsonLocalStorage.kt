b1941723e74bcd178aef68c95ef0a534eb056646
Fruxz <cedricspitzer@outlook.de>
2025-07-03 13:40:09 +0200

    feature: add local storage
    for saving json data in files, easily and on-the-fly

---
+++ src/main/kotlin/dev/fruxz/ascend/json/localstorage/JsonLocalStorage.kt
@@ -0,0 +1,67 @@
+package dev.fruxz.ascend.json.localstorage
+
+import kotlinx.serialization.Serializable
+import java.nio.file.Path
+import kotlin.reflect.KProperty
+
+/**
+ * This interface represents a local storage for JSON data, allowing you to read and write
+ * serialized objects to a file. It provides a way to manage the state of an object with
+ * default values and file paths.
+ *
+ * @param T The type of the object to be stored, which must be serializable.
+ * @author Fruxz
+ * @since 2025.7
+ */
+interface JsonLocalStorage<T : Any> {
+
+    /**
+     * A function that provides a default value for the stored object.
+     * This function should return a serializable object of type T.
+     * Used for writing & reading the first value, if no file exists.
+     * @author Fruxz
+     * @since 2025.7
+     */
+    val defaultValue: () -> (@Serializable T)
+
+    /**
+     * The path to the file where the JSON data is stored.
+     * This should be a valid [Path] object pointing to the desired storage location (file of final destination, not folder!).
+     * @author Fruxz
+     * @since 2025.7
+     */
+    val filePath: Path
+
+    /**
+     * The current state of the stored object.
+     * This is the value that is read from or written to the file.
+     * @author Fruxz
+     * @since 2025.7
+     */
+    var state: T
+
+    /**
+     * Reads the JSON data from the file and returns it as an object of type T.
+     * If the file does not exist or is empty, it returns null.
+     * @author Fruxz
+     * @since 2025.7
+     */
+    fun readFile(): T?
+
+    /**
+     * Writes the given object to the file as JSON data.
+     * Returns the path to the file where the data was written.
+     *
+     * @param value The object of type T to be written to the file.
+     * @author Fruxz
+     * @since 2025.7
+     */
+    fun writeFile(value: T): Path
+
+    operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): JsonLocalStorage<T>
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T)
+
+}
---
+++ src/main/kotlin/dev/fruxz/ascend/json/localstorage/LocalStorage.kt
@@ -0,0 +1,128 @@
+package dev.fruxz.ascend.json.localstorage
+
+import dev.fruxz.ascend.extension.createFileAndDirectories
+import dev.fruxz.ascend.extension.logging.getItsLogger
+import dev.fruxz.ascend.json.readJson
+import dev.fruxz.ascend.json.writeJson
+import kotlinx.serialization.Serializable
+import java.nio.file.Path
+import kotlin.io.path.copyTo
+import kotlin.io.path.div
+import kotlin.io.path.fileSize
+import kotlin.io.path.notExists
+import kotlin.io.path.pathString
+import kotlin.reflect.KProperty
+
+/**
+ * Creates a new [JsonLocalStorage] instance for the specified type [T].
+ * Adds [beforePush] and [afterPush] hooks for custom processing before and after saving the state.
+ * @return a new [JsonLocalStorage] instance for the specified type [T].
+ * @see JsonLocalStorage
+ * @author Fruxz
+ * @since 2025.7
+ */
+inline fun <reified T : Any> storedJson(
+    path: Path,
+    noinline beforePush: (T) -> T = { it },
+    noinline afterPush: (T) -> Unit = { },
+    noinline default: () -> (@Serializable T),
+) = object : JsonLocalStorage<T> {
+
+    var exception: Exception? = null
+
+    override val defaultValue: () -> @Serializable T = default
+    override val filePath: Path = path
+    private var cache: T? = null
+    override var state: T
+        get() = cache ?: readFile() ?: default()
+        set(value) {
+            writeFile(beforePush(value))
+            afterPush(value)
+        }
+
+    override fun readFile() = try {
+        when {
+            path.notExists() || path.fileSize() == 0L -> null
+            else -> path.readJson<T>().also { cache = it }
+        }
+    } catch (e: Exception) {
+        getItsLogger().warning { "Exception '${e.message}' on loading ${path.pathString} - defaulting" }
+        e.printStackTrace()
+        cache = default()
+        exception = e
+        null
+    }
+
+    override fun writeFile(value: T) = try {
+        createBackupFile()
+        when {
+            exception == null -> {
+                path.writeJson(value).also { cache = value }
+                deleteBackupFile()
+            }
+            else -> getItsLogger().warning { "stored object @ '${path.pathString}' of type ${value::class.qualifiedName} is in error state, not saving" }
+        }
+        path
+    } catch (e: Exception) {
+        restoreFromBackup()
+        getItsLogger().warning { "Exception '${e.message}' on saving ${path.pathString}" }
+        e.printStackTrace()
+        exception = e
+        throw e
+    }
+
+    override fun provideDelegate(thisRef: Any?, property: KProperty<*>): JsonLocalStorage<T> {
+
+        if (path.notExists() || path.fileSize() == 0L) {
+            cache = default()
+            path.createFileAndDirectories()
+            path.writeJson(cache)
+        }
+
+        return this
+    }
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+        state
+
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        state = value
+    }
+
+    private fun createBackupFile() {
+        if (path.notExists()) {
+            getItsLogger().warning { "Cannot create backup file, path does not exist: ${path.pathString}" }
+            return
+        }
+
+        val backupPath = path.parent / "${path.fileName}.old"
+        if (backupPath.notExists()) backupPath.createFileAndDirectories()
+        path.copyTo(backupPath, overwrite = true)
+    }
+
+    private fun deleteBackupFile() {
+        val backupPath = path.parent / "${path.fileName}.old"
+        if (backupPath.notExists()) {
+            getItsLogger().warning { "Cannot delete backup file, path does not exist: ${backupPath.pathString}" }
+            return
+        }
+        backupPath.toFile().delete()
+    }
+
+    private fun restoreFromBackup() {
+        val backupPath = path.parent / "${path.fileName}.old"
+        if (backupPath.notExists()) {
+            getItsLogger().warning { "Cannot restore from backup, backup file does not exist: ${backupPath.pathString}" }
+            return
+        }
+
+        try {
+            backupPath.copyTo(path, overwrite = true)
+            getItsLogger().info { "Restored from backup: ${backupPath.pathString} to ${path.pathString}" }
+        } catch (e: Exception) {
+            getItsLogger().warning { "Failed to restore from backup: ${e.message}" }
+            e.printStackTrace()
+        }
+    }
+
+}
