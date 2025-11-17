5a7b696f5e1d7dcd3b61b5ec0c139cd7fc3d55c9
Fruxz <cedricspitzer@outlook.de>
2025-11-17 18:29:13 +0100

    feature: add cached property delegate

---
+++ src/main/kotlin/dev/fruxz/ascend/extension/property/Cached.kt
@@ -0,0 +1,44 @@
+package dev.fruxz.ascend.extension.property
+
+import dev.fruxz.ascend.tool.time.calendar.Calendar
+import kotlin.reflect.KProperty
+import kotlin.time.Duration
+
+/**
+ * Cached property delegate that regenerates the value on access, after a specified [duration].
+ * @author Fruxz
+ * @since 2025.11
+ */
+data class CachedProperty<T>(
+    val duration: Duration,
+    val builder: () -> T,
+) {
+    var state: T? = null
+    var updated: Calendar? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (updated == null || updated!!.durationToNow() > duration) {
+            state = builder()
+            updated = Calendar.now()
+        }
+        return state!!
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        state = value
+        updated = Calendar.now()
+    }
+
+}
+
+/**
+ * Generates the property value on first call, or if [duration] has passed since last update.
+ * The value is cached internally between calls.
+ * HINT: Use [kotlin.reflect.KProperty0.getDelegate] to receive the last-update time
+ * @author Fruxz
+ * @since 2025.11
+ */
+fun <T> cached(
+    duration: Duration,
+    builder: () -> T,
+) = CachedProperty(duration = duration) { builder() }
