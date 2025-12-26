2cae4f8efcc8cb39724b2edb33e89d57726db337
TheFruxz <cedricspitzer@outlook.de>
2025-12-26 01:05:07 +0100

    feature: add defaultNow() column function
    adding a quick and easy way for ‚now‘ defaults on Kalendar columns
    
    Signed-off-by: TheFruxz <cedricspitzer@outlook.de>

--- src/main/kotlin/dev/fruxz/ascend/tool/time/state/KalendarUtil.kt
+++ src/main/kotlin/dev/fruxz/ascend/tool/time/state/KalendarUtil.kt
@@ -5,6 +5,7 @@ import dev.fruxz.ascend.tool.time.state.sql.PreciseKalendarColumnType
 import kotlinx.datetime.TimeZone
 import org.jetbrains.exposed.v1.core.Column
 import org.jetbrains.exposed.v1.core.Table
+import org.jetbrains.exposed.v1.core.Table.Dual.clientDefault
 
 fun Table.kalendarPrecise(
     name: String,
@@ -13,3 +14,10 @@ fun Table.kalendarPrecise(
 
 fun Table.kalendar(name: String): Column<Kalendar> =
     registerColumn(name = name, type = KalendarColumnType)
+
+/**
+ * Sets the (client) default value of this [Kalendar] column to the current time.
+ * @author Fruxz
+ * @since 2024.6
+ */
+fun Column<Kalendar>.defaultNow() = this.clientDefault { Kalendar.now() }
